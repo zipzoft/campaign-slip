393b979aeb579df37f2bd097596f6c8d0b404293
donnpnt <donn.pnt@hotmail.com>
2022-10-04 11:33:48 +0700

    Add ReportTransaction

--- internal/repository/report-repositiory.go
+++ internal/repository/report-repositiory.go
@@ -5,6 +5,7 @@ import (
 	"campiagn-slip/pkg/database"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"strings"
 )
 
 var _ ReportRepository = (*ReportRepo)(nil)
@@ -20,12 +21,12 @@ func (r ReportRepo) ReportTransaction(ctx *gin.Context, Page, PerPage int) (inte
 	dateFrom := ctx.Query("date_from")
 	dateTo := ctx.Query("date_to")
 	Prefix := ctx.Query("prefix")
-
+	ArrPrefix := strings.Split(Prefix, ",")
 	Aggregate := bson.A{
 		bson.M{"$match": bson.M{"$and": bson.A{
 			bson.M{"date_bank": bson.M{"$gte": dateFrom}},
 			bson.M{"date_bank": bson.M{"$lte": dateTo}},
-			bson.M{"prefix": bson.M{"$in": bson.A{Prefix}}},
+			bson.M{"prefix": bson.M{"$in": ArrPrefix}},
 			bson.M{"is_redeem": true},
 		}}},
 		bson.M{"$group": bson.M{"_id": "$username",
@@ -34,7 +35,7 @@ func (r ReportRepo) ReportTransaction(ctx *gin.Context, Page, PerPage int) (inte
 		},
 		},
 		bson.M{"$project": bson.M{"data.prefix": 1, "data.coin": 1, "data.date_bank": 1, "data.slip_number": 1, "data.is_redeem": 1, "data.created_at": 1, "count": 1}},
-		bson.M{"$sort": bson.M{"username": 1}},
+		bson.M{"$sort": bson.M{"created_at": 1}},
 		bson.M{"$skip": (Page - 1) * PerPage},
 		bson.M{"$limit": PerPage},
 	}
